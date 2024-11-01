26b2b3234838b60dbbf7fc74be23d03c6dfa00c7
Yunus <yunuscicekk25@gmail.com>
2024-11-01 03:32:06 +0300

    Add project files.

---
+++ SensiveProject.EntityLayer/Concrete/TagCloud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensiveProject.EntityLayer.Concrete
+{
+    public class TagCloud
+    {
+        public int TagCloudId { get; set; }
+        public string Title { get; set; }
+    }
+}
